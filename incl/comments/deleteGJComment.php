7ae9b0043c98ba7d32f5ff4e73d4ab1e28948e56
Lucy E <tflux3on@yahoo.com>
2020-06-25 15:33:10 -0700

    Actually fixed not being able to delete your comments this time

--- incl/comments/deleteGJComment.php
+++ incl/comments/deleteGJComment.php
@@ -5,13 +5,10 @@ require_once "../lib/GJPCheck.php";
 require_once "../lib/exploitPatch.php";
 $ep = new exploitPatch();
 $commentID = $ep->remove($_POST["commentID"]);
-$accountID = $ep->remove($_POST["accountID"]);
-$gjp = $ep->remove($_POST["gjp"]);
-$GJPCheck = new GJPCheck();
-$gjpresult = $GJPCheck->check($gjp,$accountID);
-if($gjpresult == 1){
-	$query = $db->prepare("SELECT userID FROM users WHERE extID = :accountID");
-	$query->execute([':accountID' => $accountID]);
+$udid = $ep->remove($_POST["udid"]);
+if($_POST['secret'] == 'Wmfd2893gb7'){
+	$query = $db->prepare("SELECT userID FROM users WHERE extID = :udid");
+	$query->execute([':udid' => $udid]);
 	$userID = $query->fetchColumn();
 	$query = $db->prepare("DELETE FROM comments WHERE commentID=:commentID AND userID=:userID LIMIT 1");
 	$query->execute([':commentID' => $commentID, ':userID' => $userID]);
@@ -25,7 +22,7 @@ if($gjpresult == 1){
 		$query = $db->prepare("SELECT extID FROM users WHERE userID = :userID");
 		$query->execute([':userID' => $creatorID]);
 		$creatorAccID = $query->fetchColumn();
-		if($creatorAccID == $accountID){
+		if($creatorAccID == $udid){
 			$query = $db->prepare("DELETE FROM comments WHERE commentID=:commentID AND levelID=:levelID LIMIT 1");
 			$query->execute([':commentID' => $commentID, ':levelID' => $levelID]);
 		}

25e526b626ca95915b7e121c3d3bc7e3095a2f90
Lucy E <superhuman741@gmail.com>
2020-06-25 11:50:17 -0700

    The Cron Job

--- tools/cron/cron.php
+++ tools/cron/cron.php
@@ -1,1 +1,27 @@
-
+<?php 
+$title = "The Cron Job";
+include "../../../incl/load_top.php" ?>
+<?php
+chdir(dirname(__FILE__));
+set_time_limit(0);
+include "fixcps.php";
+ob_flush();
+flush();
+include "autoban.php";
+ob_flush();
+flush();
+include "friendsLeaderboard.php";
+ob_flush();
+flush();
+include "removeBlankLevels.php";
+ob_flush();
+flush();
+include "songsCount.php";
+ob_flush();
+flush();
+include "fixnames.php";
+ob_flush();
+flush();
+file_put_contents("../logs/cronlastrun.txt",time());
+?>
+<?php include "../../../incl/load_bottom.php" ?>
